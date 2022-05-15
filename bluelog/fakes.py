d57b1ac1f9aaebe8f121169571909ff1293b582f
wangkeqin <wkq214@163.com>
2022-05-16 00:32:03 +0800

    备案号上传

--- bluelog/fakes.py
+++ bluelog/fakes.py
@@ -18,18 +18,18 @@ fake = Faker()
 
 def fake_admin():
     admin = Admin(
-        username='admin',
-        blog_title='Bluelog',
-        blog_sub_title="No, I'm the real thing.",
+        username='wmm',
+        blog_title='520wmb',
+        blog_sub_title="希望和宝贝长长久久❤️",
         name='Mima Kirigoe',
         about='Um, l, Mima Kirigoe, had a fun time as a member of CHAM...'
     )
-    admin.set_password('helloflask')
+    admin.set_password('wkq')
     db.session.add(admin)
     db.session.commit()
 
 
-def fake_categories(count=10):
+def fake_categories(count=1):
     category = Category(name='Default')
     db.session.add(category)
 
@@ -42,20 +42,25 @@ def fake_categories(count=10):
             db.session.rollback()
 
 
-def fake_posts(count=50):
-    for i in range(count):
-        post = Post(
-            title=fake.sentence(),
-            body=fake.text(2000),
-            category=Category.query.get(random.randint(1, Category.query.count())),
-            timestamp=fake.date_time_this_year()
-        )
-
-        db.session.add(post)
+def fake_posts(count=1):
+    # for i in range(count):
+    #     post = Post(
+    #         title=fake.sentence(),
+    #         body=fake.text(2000),
+    #         category=Category.query.get(random.randint(1, Category.query.count())),
+    #         timestamp=fake.date_time_this_year()
+    #     )
+    post = Post(
+        title='宝贝生日快乐🎂',
+        body='喜欢宝贝的每一天都很幸福',
+        category=Category.query.get(random.randint(1, Category.query.count())),
+        timestamp=fake.date_time_this_year()
+    )
+    db.session.add(post)
     db.session.commit()
 
 
-def fake_comments(count=500):
+def fake_comments(count=1):
     for i in range(count):
         comment = Comment(
             author=fake.name(),
@@ -113,9 +118,10 @@ def fake_comments(count=500):
 
 
 def fake_links():
-    twitter = Link(name='Twitter', url='#')
-    facebook = Link(name='Facebook', url='#')
-    linkedin = Link(name='LinkedIn', url='#')
-    google = Link(name='Google+', url='#')
-    db.session.add_all([twitter, facebook, linkedin, google])
+    weibo = Link(name='Weibo', url='https://weibo.com/u/6269655963')
+    # facebook = Link(name='Facebook', url='#')
+    # linkedin = Link(name='LinkedIn', url='#')
+    # google = Link(name='Google+', url='#')
+    # db.session.add_all([twitter, facebook, linkedin, google])
+    db.session.add_all([weibo])
     db.session.commit()
